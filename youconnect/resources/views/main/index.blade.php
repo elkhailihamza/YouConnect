65d2acfc080f3c4d5fdce3e118fcfe43f0933aa7
elkhailihamza <elkhailihamza331@gmail.com>
2024-02-15 20:54:13 +0100

    some stuff

--- youconnect/resources/views/main/index.blade.php
+++ youconnect/resources/views/main/index.blade.php
@@ -141,11 +141,11 @@
         </aside>
     </div>
 
-    <div id="publication" class="  w-1/2 max-h-screen overflow-y-auto overflow-hidden dark:text-white">
+    <div id="publication" class="w-1/2 max-h-screen overflow-y-auto overflow-hidden dark:text-white">
         <div class="space-y-4">
             @if (isset($posts) && $posts->isNotEmpty())
             @foreach ($posts as $post)
-            <a href="{{ route('main.posts.view', ['post' => $post]) }}">
+            <a  data-modal-target="default-modal-{{$post}}" data-modal-toggle="default-modal">
                 <div class="border rounded">
                     <div class="w-full flex items-center justify-center bg-white dark:bg-gray-800">
                         <img src="{{ asset('storage/'.$post->cover) }}" alt="Post" class="w-96 h-auto rounded-t">
@@ -153,6 +153,55 @@
                     <div class="p-4 border-t-2">
                         <h2 class="font-bold text-lg">{{ $post->title }}</h2>
                         <p>{{ $post->content }}</p>
+                    </div>
+                </div>
+
+                <div id="default-modal-{{$post}}" tabindex="-1" aria-hidden="true"
+                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative p-4 w-full max-w-2xl max-h-full">
+                        <!-- Modal content -->
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <!-- Modal header -->
+                            <div
+                                class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                    Terms of Service
+                                </h3>
+                                <button type="button"
+                                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    data-modal-hide="default-modal">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <!-- Modal body -->
+                            <div class="p-4 md:p-5 space-y-4">
+                                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                    With less than a month to go before the European Union enacts new consumer privacy
+                                    laws for its citizens, companies around the world are updating their terms of
+                                    service agreements to comply.
+                                </p>
+                                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                    The European Unions General Data Protection Regulation (G.D.P.R.) goes into effect
+                                    on May 25 and is meant to ensure a common set of data rights in the European Union.
+                                    It requires organizations to notify users as soon as possible of high-risk data
+                                    breaches that could personally affect them.
+                                </p>
+                            </div>
+                            <!-- Modal footer -->
+                            <div
+                                class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                <button data-modal-hide="default-modal" type="button"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
+                                    accept</button>
+                                <button data-modal-hide="default-modal" type="button"
+                                    class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </a>
@@ -207,7 +256,7 @@
     });
 
     document.getElementById('toggle-right-sidebar').addEventListener('click', function () {
-        document.getElementById('right-sidebar').classList.toggle('hidden');
+        document.getElemght-sidebar').classList.toggle('hidden');
     });
 </script>
 @endsection
