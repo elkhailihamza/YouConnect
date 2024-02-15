974184512b11688c8115677fbf95d7e74ce614b7
Mohamed HAKI <Mohamedhaki70@gmail.com>
2024-02-15 11:22:06 +0100

    ADD authentification system

--- youconnect/app/Models/User.php
+++ youconnect/app/Models/User.php
@@ -42,9 +42,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'avatar',
+        'bio',
     ];
 
     /**
