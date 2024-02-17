dc61610506431d19ead3d72e07f91b209c5fdec1
Mohamed HAKI <Mohamedhaki70@gmail.com>
2024-02-17 13:25:35 +0100

    add section Conversation

--- youconnect/routes/web.php
+++ youconnect/routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-<<<<<<< HEAD
 use App\Http\Controllers\HomeController;
-=======
 use App\Http\Controllers\PostController;
->>>>>>> ccb2c59d6360bf5fa9c671cad76a5c6dad9bb6a2
 use Illuminate\Support\Facades\Route;
 
 
@@ -20,21 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
-Route::get('/',[HomeController::class, 'index'])->name('index');
-Route::get('/home',[HomeController::class, 'index'])->name('index');
-=======
+Route::get('/',[PostController::class, 'index'])->name('index');
+Route::get('/home',[PostController::class, 'index'])->name('index');
 Route::get('/', function () {
     return view('main.index');
 })->name('index');
->>>>>>> ccb2c59d6360bf5fa9c671cad76a5c6dad9bb6a2
 
 Route::get('/explore', function () {
     return view('explore');
 })->name('explore');
 Route::middleware(['auth'])->group(function () {
     Route::controller(PostController::class)->group(function () {
-        Route::get('/posts/create', 'create')->name('main.posts');
+        Route::get('/posts/create', 'createPost')->name('main.posts');
         Route::post('/posts/create/store', 'store')->name('main.posts.store');
     });
 });
