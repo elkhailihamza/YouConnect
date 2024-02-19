4b30af995c95aa0880726044cf7cb3655b527190
elkhailihamza <elkhailihamza331@gmail.com>
2024-02-19 11:24:27 +0100

    fixed dropdown menus

--- youconnect/resources/views/main/index.blade.php
+++ youconnect/resources/views/main/index.blade.php
@@ -10,17 +10,60 @@
         @if (isset($posts) && $posts->isNotEmpty())
         @foreach ($posts as $post)
         <div class="rounded shadow-md w-[680px] bg-[#FFFFFF] dark:bg-[#242526]">
-            <div class="p-4">
-                <div class="flex self-start justify-self-start w-40">
-                    <img src="https://via.placeholder.com/50" alt="User" class="w-[40px] h-[40px] rounded-full mr-2">
-                    <div class="grid">
-                        <div><span class="dark:text-white text-[15px] font-medium">{{
-                                $post->user->username }}</span></div>
-                        <span class="text-[13px] w-44 text-stone-500">{{ $post->created_at }}</span>
+            <div class="p-4 flex justify-between">
+                <div>
+                    <div class="flex self-start justify-self-start w-40">
+                        <img src="https://via.placeholder.com/50" alt="User"
+                            class="w-[40px] h-[40px] rounded-full mr-2">
+                        <div class="grid">
+                            <div><span class="dark:text-white text-[15px] font-medium">{{
+                                    $post->user->username }}</span></div>
+                            <span class="text-[13px] w-44 text-stone-500">{{ $post->created_at }}</span>
+                        </div>
+                    </div>
+                    <div class="flex justify-between ms-2 mt-1">
+                        <h2 class="text-[13px]">{{ $post->content }}</h2>
                     </div>
                 </div>
-                <div class="flex justify-between ms-2 mt-1">
-                    <h2 class="text-[13px]">{{ $post->content }}</h2>
+                <div>
+                    <button id="dropdown" data-dropdown-toggle="post-{{$post->id.'-'.$post->user->username}}"
+                        class="inline-flex w-full justify-center gap-x-1.5 rounded-md text-sm font-semibold text-gray-900"
+                        type="button"><svg fill="#000000" class="dark:fill-white" xmlns="http://www.w3.org/2000/svg"
+                            height="24" viewBox="0 -960 960 960" width="24">
+                            <path
+                                d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
+                        </svg>
+                    </button>
+
+                    <!-- Dropdown menu -->
+                    <div id="post-{{$post->id.'-'.$post->user->username}}"
+                        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="dropdown">
+                            <li>
+                                <a href="#"
+                                    class="block flex gap-1 px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><svg
+                                        xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                        fill="none" stroke="#008B00" stroke-width="1.5" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                        <circle cx="8.5" cy="7" r="4"></circle>
+                                        <line x1="20" y1="8" x2="20" y2="14"></line>
+                                        <line x1="23" y1="11" x2="17" y2="11"></line>
+                                    </svg> Edit</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="block flex gap-1 px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><svg
+                                        xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                        fill="none" stroke="#FF0000" stroke-width="1.5" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                        <circle cx="12" cy="12" r="10"></circle>
+                                        <line x1="4.93" y1="4.93" x2="19.07" y2="19.07"></line>
+                                    </svg>Delete</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             @if ($post->cover != null)
