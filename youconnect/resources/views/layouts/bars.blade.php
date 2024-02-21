5489461f6b5907645446e4f44e5cfc9c4b74cce7
elkhailihamza <elkhailihamza331@gmail.com>
2024-02-21 11:19:23 +0100

    fixed right sidebar, has diffrent bg than content's

--- youconnect/resources/views/layouts/bars.blade.php
+++ youconnect/resources/views/layouts/bars.blade.php
@@ -2,54 +2,51 @@
 <div class=" shadow-md container-xl flex justify-between ">
     <div class="fixed inset-0 mt-[70px] w-60">
         {{--left-sidebar--}}
-        <div id="left-sidebar"
-            class="lg:w-60 md:w-full h-full left-0 mr-4 hidden md:block dark:border-white "
-            >
+        <div id="left-sidebar" class="lg:w-60 md:w-full h-full left-0 mr-4 hidden md:block dark:border-white ">
             <div class="h-full px-3 py-2 pb-4 overflow-y-auto bg-white dark:bg-[#242526]">
                 <ul class="space-y-2 font-medium">
                     <div class="text-center w-full p-1">
                         <h1 class="text-gray-800 dark:text-white">actions</h1>
                     </div>
                     <hr>
-                    @auth 
+                    @auth
                     <li>
-                       
-                            <a  href="{{ route('profiles.profile', Auth::user()) }}"class="flex mx-1 items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <i class="fa-solid fa-user"></i><span class="mx-7">MY PROFILE</span>
-                            </a> 
-                                                  
+                        <a href="{{ route('profiles.profile', Auth::user()) }}"
+                            class="flex mx-1 items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <i class="fa-solid fa-user"></i><span class="mx-7">MY PROFILE</span>
+                        </a>
                     </li>
                     <li>
+                        <a href="{{ route('profiles.Myposts', ['user' => Auth::user()]) }}"
+                            class="flex  items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <i class="fa-brands fa-medium"></i><span class="mx-7">MY POSTS</span>
+                        </a>
+                    </li>
+                    @endauth
 
-                            <a  href="{{ route('profiles.Myposts', ['user' => Auth::user()]) }}"class="flex  items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <i class="fa-brands fa-medium"></i><span class="mx-7">MY POSTS</span>
-                            </a>                        
-                    </li>
-                    @endauth 
-                    
                 </ul>
             </div>
         </div>
     </div>
     {{--right-sidebar--}}
     <div id="right-sidebar"
-        class="shadow-md fixed justify-self-end h-full right-0 mt-[65px] bg-white hidden md:block overflow-y-scroll dark:text-white dark:bg-gray-800 border-gray-600 dark:border-white"
-       >
+        class="shadow-md dark:bg-[#242526] fixed justify-self-end h-full right-0 mt-[65px] bg-white hidden md:block overflow-y-scroll dark:text-white border-gray-600 dark:border-white">
         <div class="rounded w-30 p-4 mb-4 text-center dark:bg-[#242526]">
             <h2 class="font-bold mb-2">Suggested for you</h2>
             <hr>
             <ul>
                 @foreach ($users as $user)
-                 <li
+                <li
                     class="mb-2 flex justify-between items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ">
                     <div class="flex self-start justify-self-start w-40">
                         @if (!empty($user->avatar))
-    <img src="{{ asset('storage/' . $user->avatar) }}" alt="User" class="w-8 h-8 rounded-full mr-2">
-@else
-    <img src="https://via.placeholder.com/50" alt="User" class="w-8 h-8 rounded-full mr-2">  
-@endif
-                        <a style="cursor: pointer;" href="http://127.0.0.1:8000/chatify/{{$user->id}}"><div class="overflow-hidden overflow-ellipsis "><span class="dark:text-white truncate">{{
-                                $user->name }}</span></div>
+                        <img src="{{ asset('storage/' . $user->avatar) }}" alt="User" class="w-8 h-8 rounded-full mr-2">
+                        @else
+                        <img src="https://via.placeholder.com/50" alt="User" class="w-8 h-8 rounded-full mr-2">
+                        @endif
+                        <a style="cursor: pointer;" href="http://127.0.0.1:8000/chatify/{{$user->id}}">
+                            <div class="overflow-hidden overflow-ellipsis w-[125px]"><span class="dark:text-white truncate">{{
+                                    $user->name }}</span></div>
                     </div></a>
                     <div class="relative inline-block text-left">
 
@@ -65,8 +62,7 @@
                         <!-- Dropdown menu -->
                         <div id="user-{{$user->name.'-'.$user->id}}"
                             class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                aria-labelledby="dropdown">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown">
                                 <li>
                                     <a href="#"
                                         class="block flex gap-1 px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><svg
