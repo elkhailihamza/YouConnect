fab05c176d79e5a8b6bf820bbf03b6869a51b09c
Mohamed HAKI <Mohamedhaki70@gmail.com>
2024-02-24 00:13:26 +0100

    frentend friend requests

--- youconnect/resources/views/Profiles/friendrequest.blade.php
+++ youconnect/resources/views/Profiles/friendrequest.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 <div  class="mt-20 mb-12 max-h-screen container-xl dark:text-white ">
-    <div class="rounded shadow-md bg-[#FFFFFF] dark:bg-[#242526]">
+    <div class="rounded shadow-md  dark:bg-[#242526]">
         @if ($receivedRequests->isEmpty())
             <p>Aucune demande reçue pour le moment.</p>
         @else
             @foreach ($receivedRequests as $request)
-                <div class="flex items-center justify-between mb-4">
+                <div class="flex items-center justify-between ">
                     <div class="flex items-center">
-                        <div class="bg-white rounded-lg shadow-md p-4">
-                            <p class="text-lg font-semibold">{{ $request->sender->name }}</p>
+                        <div class=" rounded-lg shadow-md p-4">
+                            <p class="text-lg font-semibold dark:white">{{ $request->sender->name }}</p>
                             <img src="{{ $request->sender->avatar }}" alt="">
                             <p class="text-gray-600">{{ $request->created_at->format('d/m/Y H:i') }}</p>
                         </div>
                     </div>
                     <div class="mt-5 ms-3">
                         <div class="h-16 border-t select-none rounded-b flex text-center items-center justify-around bg-white dark:bg-[#242526]">
-                            <div class="flex justify-end mt-4">
+                            <div class="flex justify-end ">
                                 <!-- Boutons pour accepter ou rejeter l'invitation -->
                                 <form action="{{ route('accept-request', $request->id) }}" method="post">
                                     @csrf
